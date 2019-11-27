c467f4af9e2068b1f0c63dfabeb8d991131b57ad
Alex <candelarioac@gmail.com>
2019-11-27 13:07:26 -0500

    added schema and connected to db

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
+//Connect to database
+connectDB();
+
+//init middleware
+
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the contact-keeper api" })
