5ecc1c2a358096ba5e1df3683ae3d725d4d2b7b4
Alex Candelario <alexcandelario@DESKTOP-6Q4SQGG.home>
2019-11-27 22:04:47 -0500

    created the post req for log in

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { check, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs')
 // @route  POST api/users
 // @desc   Register a user
 // @access Public
@@ -12,14 +13,33 @@ router.post(
       .not()
       .isEmpty(),
     check("email", "Please enter a valid email").isEmail(),
-    check("password", "Please enter correct password").isLength({ min: 6 })
+    check("password", "Please enter co`rrect password").isLength({ min: 6 })
   ],
-  (req, res) => {
+   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
 
-    res.send("passed");
+    const { name, email, password} = req.body;
+
+    try {
+      let user = await User.findOne({email});
+      if(user) return res.status(400).json({msg: 'User already exists'})
+
+      user = new User({name, email, password}); 
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User Save');
+
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error')
+    }
   }
 );
 
